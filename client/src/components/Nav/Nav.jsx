a4d2ae5639ba5a660d6f00c9d4760b2e59ddc8ce
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-01-11 19:28:04 -0800

    Initial markup in place.

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from '../../assets/logo/InStock-Logo.svg'
 
 export default function Nav() {
     return (
-        <div>
-        </div>
+        <header className="nav">
+            <div className="nav__block">
+                <Link className="nav__block-logo" to="/">
+                    <img className="nav__block-logo-img" src={Logo} alt="Instock Logo"/>
+                </Link>
+                <div className="nav__block-list">
+                    <Link className="nav__block-list-item nav__block-list-item--current" to="/warehouses">Warehouses</Link>
+                    <Link className="nav__block-list-item" to="/inventory">Inventory</Link>
+                </div>
+            </div>
+        </header>
     )
 }
