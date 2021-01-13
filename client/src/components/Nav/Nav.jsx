0d3fe429f864cee73a2e1a00e8371a7983950174
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-01-12 18:44:08 -0800

    Revert "Nk 27 - Header component"

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,36 +1,8 @@
-import React, { Component } from 'react';
-import './Nav.scss';
-import { NavLink } from 'react-router-dom';
-import Logo from '../../assets/logo/InStock-Logo.svg'
+import React from 'react'
 
-
-export default class Nav extends Component {
-  
-  componentDidMount() {
-    // console.log(this.props.path)
-  }
-
-  render() {
+export default function Nav() {
     return (
-      <header className="nav">
-        <div className="nav__block">
-          <NavLink className="nav__block-logo" to="/">
-            <img className="nav__block-logo-img" src={Logo} alt="Instock Logo"/>
-          </NavLink>
-          <div className="nav__block-list">
-            <NavLink 
-              className="nav__block-list-item"
-              activeClassName="nav__block-list-item--active"
-              to="/warehouses">Warehouses
-            </NavLink>
-            <NavLink 
-              className="nav__block-list-item"
-              activeClassName="nav__block-list-item--active"
-              to="/inventory">Inventory
-            </NavLink>
-          </div>
+        <div>
         </div>
-      </header>
     )
-  }
 }
