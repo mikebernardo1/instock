98c76f2feb697b2e006781d44931a651012b5207
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-01-12 18:21:40 -0800

    Styled header for all layouts.

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Nav.scss';
-import { Link } from 'react-router-dom';
-
+import { NavLink } from 'react-router-dom';
 import Logo from '../../assets/logo/InStock-Logo.svg'
 
-export default function Nav() {
-  return (
-    <header className="nav">
-      <div className="nav__block">
-        <Link className="nav__block-logo" to="/">
-          <img className="nav__block-logo-img" src={Logo} alt="Instock Logo"/>
-        </Link>
-        <div className="nav__block-list">
-          <Link className="nav__block-list-item nav__block-list-item--active" to="/warehouses">Warehouses</Link>
-          <Link className="nav__block-list-item" to="/inventory">Inventory</Link>
+
+export default class Nav extends Component {
+  
+  componentDidMount() {
+    // console.log(this.props.path)
+  }
+
+  render() {
+    return (
+      <header className="nav">
+        <div className="nav__block">
+          <NavLink className="nav__block-logo" to="/">
+            <img className="nav__block-logo-img" src={Logo} alt="Instock Logo"/>
+          </NavLink>
+          <div className="nav__block-list">
+            <NavLink 
+              className="nav__block-list-item"
+              activeClassName="nav__block-list-item--active"
+              to="/warehouses">Warehouses
+            </NavLink>
+            <NavLink 
+              className="nav__block-list-item"
+              activeClassName="nav__block-list-item--active"
+              to="/inventory">Inventory
+            </NavLink>
+          </div>
         </div>
-      </div>
-    </header>
-  )
+      </header>
+    )
+  }
 }
- 
