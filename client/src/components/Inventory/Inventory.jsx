6b85428540d30c7fff9750ffe47569bd6fd38b23
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-01-14 19:05:34 -0800

    Initial commit

--- client/src/components/Inventory/Inventory.jsx
+++ client/src/components/Inventory/Inventory.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-export default function Inventory() {
-    return (
-        <div>
-        </div>
-    )
+function Inventory() {
+  return (
+    <div>
+      
+    </div>
+  )
 }
+
+export default Inventory
