eaecc32e66b83cc6ab4cb4932cefdcdaeed4f90a
Michael Bernardo <michael_bernardo@me.com>
2021-01-15 20:28:30 -0500

    nk-12 mobile scss non functional

--- client/src/components/InventoryAddItem/InventoryAddItem.jsx
+++ client/src/components/InventoryAddItem/InventoryAddItem.jsx
@@ -13,46 +13,49 @@ export default function InventoryAddItem() {
                 <h2 className="inventoryAddItem__div2-header">Item Details</h2>
                 <div className="inventoryAddItem__div2-block">
                     <h3 className="inventoryAddItem__div2-block-categoryTitle">Item Name</h3>
-                    <input type="text" placeholder="Item Name" className="inventoryAddItem__div2-block-textfield1"></input>
+                    <input type="text" placeholder="Item Name" className="inventoryAddItem__div2-block-textField1"></input>
                 </div>
                 <div className="inventoryAddItem__div2-block">
                     <h3 className="inventoryAddItem__div2-block-categoryTitle">Description</h3>
-                    <input type="text" placeholder="Please enter a brief descrption" className="inventoryAddItem__div2-block-textfield2"></input>
+                    <textarea placeholder="Please enter a brief descrption" className="inventoryAddItem__div2-block-textField2"></textarea>
                 </div>
                 <div className="inventoryAddItem__div2-block">
                     <h3 className="inventoryAddItem__div2-block-categoryTitle">Category</h3>
                     <select>
-                        <option>Electronics</option>
+                        <option className="inventoryAddItem__div2-block-option">Electronics</option>
                     </select>
                 </div>
             </div>
             <div className="inventoryAddItem__div3">
-                <h2>Item Availability</h2>
-                <div>
-                    <h3>Status</h3>
-                    <div>
-                        <input type="radio"></input>
-                        <label>In stock</label>
-                        <input type="radio"></input>
-                        <label>Out of stock</label>
+                <h2 className="inventoryAddItem__div3-header">Item Availability</h2>
+                <div className="inventoryAddItem__div3-mainSection">
+                    <h3 className="inventoryAddItem__div3-block-categoryTitle">Status</h3>
+                    <div className="inventoryAddItem__div3-block inventoryAddItem__div3-block-buttons">
+                        <div className="inventoryAddItem__div3-block-buttons-div1">
+                            <input type="radio" className="inventoryAddItem__div3-block-buttons-div1-radio" name="status"></input>
+                            <label className="inventoryAddItem__div3-block-buttons-div1-label">In stock</label>
+                        </div>
+                        <div className="inventoryAddItem__div3-block-buttons-div2">
+                            <input type="radio" className="inventoryAddItem__div3-block-buttons-div2-radio" name="status"></input>
+                            <label className="inventoryAddItem__div3-block-buttons-div1-label">Out of stock</label>
+                        </div>
                     </div>
-                    <div>
-                        <h3>Quantity</h3>
-                        <input type="text"></input>
+                    <div className="inventoryAddItem__div3-block">
+                        <h3 className="inventoryAddItem__div3-block-categoryTitle">Quantity</h3>
+                        <input type="text" className="inventoryAddItem__div2-block-textField1"></input>
                     </div>
-                    <div>
-                        <h3>Warehouse</h3>
+                    <div className="inventoryAddItem__div3-block">
+                        <h3 className="inventoryAddItem__div3-block-categoryTitle">Warehouse</h3>
                         <select>
-                            <option disabled defaultValue>Please select</option>
+                            <option disabled defaultValue className="inventoryAddItem__div2-block-option">Please select</option>
                         </select>
-                    </div>
-                    <div>
-                        <button>Cancel</button>
-                        <button>+ Add Item</button>
                     </div>
                 </div>
             </div>
-
+            <div className="inventoryAddItem__div4">
+                <button className="inventoryAddItem__div4-button inventoryAddItem__div4-button--white">Cancel</button>
+                <button className="inventoryAddItem__div4-button inventoryAddItem__div4-button--blue">+ Add Item</button>
+            </div>
         </div>
     )
 }
