830130c22e5ad4eeabe696c2df06005443ab106b
Michael Bernardo <michael_bernardo@me.com>
2021-01-16 10:02:08 -0500

    nk-12 tablet and desktop view

--- client/src/components/InventoryAddItem/InventoryAddItem.jsx
+++ client/src/components/InventoryAddItem/InventoryAddItem.jsx
@@ -9,46 +9,50 @@ export default function InventoryAddItem() {
                 <img src={arrowBack} alt="arrow back" className="inventoryAddItem__div1-arrow"></img>
                 <h1 className="inventoryAddItem__div1-title">Add New Inventory Item</h1>
             </div>
-            <div className="inventoryAddItem__div2">
-                <h2 className="inventoryAddItem__div2-header">Item Details</h2>
-                <div className="inventoryAddItem__div2-block">
-                    <h3 className="inventoryAddItem__div2-block-categoryTitle">Item Name</h3>
-                    <input type="text" placeholder="Item Name" className="inventoryAddItem__div2-block-textField1"></input>
-                </div>
-                <div className="inventoryAddItem__div2-block">
-                    <h3 className="inventoryAddItem__div2-block-categoryTitle">Description</h3>
-                    <textarea placeholder="Please enter a brief descrption" className="inventoryAddItem__div2-block-textField2"></textarea>
-                </div>
-                <div className="inventoryAddItem__div2-block">
-                    <h3 className="inventoryAddItem__div2-block-categoryTitle">Category</h3>
-                    <select>
-                        <option className="inventoryAddItem__div2-block-option">Electronics</option>
-                    </select>
-                </div>
-            </div>
-            <div className="inventoryAddItem__div3">
-                <h2 className="inventoryAddItem__div3-header">Item Availability</h2>
-                <div className="inventoryAddItem__div3-mainSection">
-                    <h3 className="inventoryAddItem__div3-block-categoryTitle">Status</h3>
-                    <div className="inventoryAddItem__div3-block inventoryAddItem__div3-block-buttons">
-                        <div className="inventoryAddItem__div3-block-buttons-div1">
-                            <input type="radio" className="inventoryAddItem__div3-block-buttons-div1-radio" name="status"></input>
-                            <label className="inventoryAddItem__div3-block-buttons-div1-label">In stock</label>
-                        </div>
-                        <div className="inventoryAddItem__div3-block-buttons-div2">
-                            <input type="radio" className="inventoryAddItem__div3-block-buttons-div2-radio" name="status"></input>
-                            <label className="inventoryAddItem__div3-block-buttons-div1-label">Out of stock</label>
-                        </div>
+            <div className="inventoryAddItem__mainContent">
+                <div className="inventoryAddItem__div2">
+                    <h2 className="inventoryAddItem__div2-header">Item Details</h2>
+                    <div className="inventoryAddItem__div2-block">
+                        <h3 className="inventoryAddItem__div2-block-categoryTitle">Item Name</h3>
+                        <input type="text" placeholder="Item Name" className="inventoryAddItem__div2-block-textField1"></input>
                     </div>
-                    <div className="inventoryAddItem__div3-block">
-                        <h3 className="inventoryAddItem__div3-block-categoryTitle">Quantity</h3>
-                        <input type="text" className="inventoryAddItem__div2-block-textField1"></input>
+                    <div className="inventoryAddItem__div2-block">
+                        <h3 className="inventoryAddItem__div2-block-categoryTitle">Description</h3>
+                        <textarea placeholder="Please enter a brief descrption" className="inventoryAddItem__div2-block-textField2"></textarea>
                     </div>
-                    <div className="inventoryAddItem__div3-block">
-                        <h3 className="inventoryAddItem__div3-block-categoryTitle">Warehouse</h3>
+                    <div className="inventoryAddItem__div2-block">
+                        <h3 className="inventoryAddItem__div2-block-categoryTitle">Category</h3>
                         <select>
-                            <option disabled defaultValue className="inventoryAddItem__div2-block-option">Please select</option>
+                            <option className="inventoryAddItem__div2-block-option">Electronics</option>
                         </select>
+                    </div>
+                </div>
+                <div className="inventoryAddItem__div3">
+                    <h2 className="inventoryAddItem__div3-header">Item Availability</h2>
+                    <div className="inventoryAddItem__div3-mainSection">
+                        <div className="inventoryAddItem__div3-block inventoryAddItem__div3-block-buttons">
+                            <h3 className="inventoryAddItem__div3-categoryTitle">Status</h3>
+                            <div className="inventoryAddItem__div3-block-buttons--flex">
+                                <div className="inventoryAddItem__div3-block-buttons-div1">
+                                    <input type="radio" className="inventoryAddItem__div3-block-buttons-div1-radio" name="status"></input>
+                                    <label className="inventoryAddItem__div3-block-buttons-div1-label">In stock</label>
+                                </div>
+                                <div className="inventoryAddItem__div3-block-buttons-div2">
+                                    <input type="radio" className="inventoryAddItem__div3-block-buttons-div2-radio" name="status"></input>
+                                    <label className="inventoryAddItem__div3-block-buttons-div1-label">Out of stock</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="inventoryAddItem__div3-block">
+                            <h3 className="inventoryAddItem__div3-block-categoryTitle">Quantity</h3>
+                            <input type="text" className="inventoryAddItem__div2-block-textField1"></input>
+                        </div>
+                        <div className="inventoryAddItem__div3-block">
+                            <h3 className="inventoryAddItem__div3-block-categoryTitle">Warehouse</h3>
+                            <select>
+                                <option disabled defaultValue className="inventoryAddItem__div2-block-option">Please select</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
             </div>
